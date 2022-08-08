b90f42da086fc9dc2cb61b68d64080f8ffd69fff
Dylan Roman <romand.buitrago@gmail.com>
2022-08-08 14:27:13 -0400

    cleaning and main.exe

--- main-win.c
+++ main-win.c
@@ -2,15 +2,12 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
-#include <string.h>
 
 #define LENGTH 100
 #define ROW 10
 
 int board[LENGTH] = {};
-char *showBoard[LENGTH] = {};
-bool gameOver = false;
+int gameOver = 0;
 int tries = 0, position = 0, hit = 0;
 
 void initBoards()
@@ -26,8 +23,8 @@ void initBoards()
   {
     time_t t;
 
-    bool added = false;
-    while (!added)
+    int added = 0;
+    while (added == 0)
     {
       srand((unsigned)time(&t) + i);
       int position = rand() % LENGTH;
@@ -37,47 +34,21 @@ void initBoards()
         board[position] = 3;
         board[position + 1] = 3;
         board[position + 2] = 3;
-        added = true;
+        added = 1;
       }
       else if (i == 1 && board[position + ROW] == 0 && position < LENGTH - ROW)
       {
         board[position] = 2;
         board[position + ROW] = 2;
-        added = true;
+        added = 1;
       }
       else if (i == 2 && board[position] == 0)
       {
         board[position] = 1;
-        added = true;
+        added = 1;
       }
     }
   }
-
-  // ESTO ES PARA DESARROLLO, EN PRODUCTION SE PONE SIMPLEMENTE EN 0
-  for (int i = 0; i < LENGTH; i++)
-  {
-    if (board[i] == 0)
-    {
-      showBoard[i] = "0";
-    }
-    else if (board[i] == 1)
-    {
-      showBoard[i] = "1";
-    }
-    else if (board[i] == 2)
-    {
-      showBoard[i] = "2";
-    }
-    else
-    {
-      showBoard[i] = "3";
-    }
-  }
-
-  // for (int i = 0; i < LENGTH; i++)
-  // {
-  //   showBoard[i] = "0";
-  // }
 }
 
 void render()
@@ -90,20 +61,20 @@ void render()
     {
       printf("\x1b[31m+\x1b[0m\t");
     }
-    // else if (showBoard[i] == 4)
-    // {
-    //   printf("\t\x1b[36m*\x1b[0m");
-    // }
-    // else if (showBoard[i] == 5)
-    // {
-    //   printf("\t\x1b[32mX\x1b[0m");
-    // } else {
-    //   printf("\t0");
-    // }
-
+    else if (board[i] == 4)
+    {
+      printf("\x1b[36m*\x1b[0m\t");
+    }
+    else if (board[i] == 5)
+    {
+      printf("\x1b[32mX\x1b[0m\t");
+      // } else {
+      //   printf("%d\t", board[i]);
+      // }
+    }
     else
     {
-      printf("%s\t", showBoard[i]);
+      printf("0\t");
     }
 
     if ((i + 1) % ROW == 0)
@@ -118,7 +89,7 @@ void render()
 
 void playAgain()
 {
-  gameOver = false;
+  gameOver = 0;
   tries = 0;
   position = 0;
   hit = 0;
@@ -130,7 +101,7 @@ void playAgain()
 void renderOptions(int win)
 {
   int e = 0;
-  bool selectedOption = false;
+  int selectedOption = 0;
   system("cls");
 
   if (win == 1)
@@ -145,7 +116,7 @@ void renderOptions(int win)
   printf("\x1b[33m=>\x1b[0mJUGAR DE NUEVO\n");
   printf("  SALIR\n");
 
-  while (!selectedOption)
+  while (selectedOption == 0)
   {
     if (kbhit())
     {
@@ -182,7 +153,7 @@ void renderOptions(int win)
       }
       else if (key == '\r')
       {
-        selectedOption = true;
+        selectedOption = 1;
         if (e == 0)
         {
           playAgain();
@@ -190,7 +161,7 @@ void renderOptions(int win)
         else
         {
           printf("ADIOS\n");
-          gameOver = true;
+          gameOver = 1;
         }
       }
     }
@@ -203,57 +174,46 @@ void shot()
   switch (board[position])
   {
   case 1:
-    showBoard[position] = "\x1b[36m*\x1b[0m";
-    board[position] = 0;
+    board[position] = 4;
     break;
 
   case 2:
-    showBoard[position] = "\x1b[36m*\x1b[0m";
-    board[position] = 0;
+    board[position] = 4;
     if (board[position + ROW] == 2)
     {
-      showBoard[position + ROW] = "\x1b[36m*\x1b[0m";
-      board[position + ROW] = 0;
+      board[position + ROW] = 4;
     }
     else
     {
-      showBoard[position - ROW] = "\x1b[36m*\x1b[0m";
-      board[position - ROW] = 0;
+      board[position - ROW] = 4;
     }
     break;
 
   case 3:
-    showBoard[position] = "\x1b[36m*\x1b[0m";
-    board[position] = 0;
+    board[position] = 4;
     if (board[position - 1] == 3)
     {
-      showBoard[position - 1] = "\x1b[36m*\x1b[0m";
-      board[position - 1] = 0;
+      board[position - 1] = 4;
 
       if (board[position - 2] == 3)
       {
-        showBoard[position - 2] = "\x1b[36m*\x1b[0m";
-        board[position - 2] = 0;
+        board[position - 2] = 4;
       }
       else
       {
-        showBoard[position + 1] = "\x1b[36m*\x1b[0m";
-        board[position + 1] = 0;
+        board[position + 1] = 4;
       }
     }
     else
     {
-      showBoard[position + 1] = "\x1b[36m*\x1b[0m";
-      board[position + 1] = 0;
-      showBoard[position + 2] = "\x1b[36m*\x1b[0m";
-      board[position + 2] = 0;
+      board[position + 1] = 4;
+      board[position + 2] = 4;
     }
     break;
 
   default:
     hit--;
-    showBoard[position] = "\x1b[32mX\x1b[0m";
-    board[position] = 0;
+    board[position] = 5;
     break;
   }
 
@@ -268,7 +228,7 @@ void inputCheck()
   if (kbhit())
   {
     char key = getch();
-    printf("%c", key);
+    // printf("%c", key);
     switch (key)
     {
     case 'd':
@@ -330,7 +290,7 @@ void main()
   system("cls");
   initBoards();
   render();
-  while (!gameOver)
+  while (gameOver == 0)
   {
     inputCheck();
     if (hit == 3)
